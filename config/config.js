7f63363bb600746d72f588cb6c223ecef5b1e607
YohanNayanajith <yohannayanajith40@gmail.com>
2024-05-07 09:53:44 +0530

    task/Add those configs

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
-const BASE_URL = "https://36b4-34-171-199-221.ngrok-free.app";
+const BASE_URL = "https://5ebc-35-239-187-47.ngrok-free.app";
 
-module.exports = { BASE_URL };
+const BENIFIT_BASE_URL = "https://a25c-34-30-66-247.ngrok-free.app";
+
+module.exports = { BASE_URL, BENIFIT_BASE_URL };
